360bd5614a1069f599f0678d8528f1c22c1ee734
pengyw97 <pengyw97@gmail.com>
2023-03-01 15:45:19 +0800

    feat: 使用流返回响应

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('node:http')
 const fs = require('node:fs')
 
-const html = fs.readFileSync('./index.html')
-
-const server = http.createServer((req, res) => res.end(html))
+const server = http.createServer((req, res) => {
+  fs.createReadStream('./index.html').pipe(res)
+})
 
 server.listen(8000, () => {
   console.log('Listening 8000')
